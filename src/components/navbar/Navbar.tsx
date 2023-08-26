3507377fadc949d7ae1ca8a3e412b0de67e0306b
mujtabadustox <i190577@nu.edu.pk>
2023-08-26 05:55:56 +0500

    mobile fix

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -36,20 +36,20 @@ const Navbar = () => {
         <span className="font-bold text-[20px] text-purple-700">{span1}</span>{myName}<span className="font-bold text-[20px] text-purple-700">{span2}</span>
       </Link>
     </div>
-    <div className="block lg:hidden">
+    <div className="sticky lg:hidden">
       <button
         onClick={() => setIsOpen(!isOpen)}
         className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
       >
         <svg
-          className={`fill-current h-3 w-3 ${isOpen ? "hidden" : "block"}`}
+          className={`fill-current h-3 w-3 ${isOpen ? "hidden" : "sticky"}`}
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
         </svg>
         <svg
-          className={`fill-current h-3 w-3 ${isOpen ? "block" : "hidden"}`}
+          className={`fill-current h-3 w-3 ${isOpen ? "sticky" : "hidden"}`}
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
         >
@@ -58,7 +58,7 @@ const Navbar = () => {
       </button>
     </div>
     <div
-      className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? "block" : "hidden"}`}
+      className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? "sticky" : "hidden"}`}
     >
 
 <div className="md:pt-0 pt-9 mx-5 my-2.5 space-x-5">
