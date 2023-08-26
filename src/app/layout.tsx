686068d8a38324d471543a049be9bd912d6612c3
mujtabadustox <i190577@nu.edu.pk>
2023-08-26 05:58:47 +0500

    mobile fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <LinkProvider>
         <div className="mx-auto sm:w-full md:min-w-[calc(100dvw)] min-h-[calc(100dvh)] flex-row bg-black py-[10px]">
-            <div className={`w-full h-[50px] text-white sticky top-3 x-navbar-fixed-top`}>
+            <div className="w-full h-[50px] text-white sticky top-3">
               <Navbar />
             </div>
             <div className="sm:w-full md:min-w-[calc(100dvw)] min-h-[calc(100dvh)] ">{children}</div>
